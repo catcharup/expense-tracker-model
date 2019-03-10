bdf1fd3ce2b024758f1d672c70a4e7e57b7afa3d
Arup <catcharup@gmail.com>
2019-03-11 02:01:21 +0530

    First commit for expense model

---
+++ src/main/java/com/expense/tracker/model/CreditCardExpenseDetail.java
@@ -0,0 +1,33 @@
+package com.expense.tracker.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Getter
+@Setter
+public class CreditCardExpenseDetail {
+
+	private @NotNull Long transactionId;
+	private @NotNull String cardId;
+	private @NotNull String bank;
+	private @NotNull LocalDate transactionDate;
+	private @NotNull BigDecimal amount;
+	private String purpose;
+	private @NotNull String transactionType;
+	
+	
+}
